64ad4569f421bce4aecdd5c72f05edc619bc4546
Brian Case <aristekrat@gmail.com>
2013-10-08 00:02:07 -0600

    Back end working locally but not yet working remotely.

--- public/javascripts/donateware-utility.js
+++ public/javascripts/donateware-utility.js
@@ -1,23 +1,12 @@
 $(document).ready(function() {
-	$('#mainSearchResults, #mainUploadForm').hide();
+	$('#mainSearchResults').hide();
 	$('#mainSearchPanel form').submit(function(e) {
 		e.preventDefault();
 		$('#mainSearchResults').show();
-		$('#gitHubNote').css('margin-top', '-1.7%');
-	});
-	$('#uploadButton').click(function(e) {
-		e.preventDefault();
-		$('#mainSearchPanel, #mainSearchResults').hide();
-		$('#mainUploadForm').show();
-		$('#gitHubNote').css('margin-top', '-1.7%');
 	});
 
-  var today = new Date();
-    var BigDay = new Date("September 09, 2013");
-    var msPerDay = 24 * 60 * 60 * 1000 ;
-    var timeLeft = (BigDay.getTime() - today.getTime());
-    var e_daysLeft = timeLeft / msPerDay;
-    var daysLeft = Math.floor(e_daysLeft);
-      $("#countdown").text(daysLeft);
-
+/*	$('#dUploadForm').submit(function(e)) {
+		$('#dUploadForm').append("<h1>Is Working!</h1>");
+	}
+*/
 });
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,13 @@
  */
 
 exports.index = function(req, res){
-  res.render('../views/index', { title: 'DonateWare' });
+  	res.render('../views/index', { title: 'DonateWare' });
 };
 
 exports.prototype = function(req, res) {
 	res.render('../views/prototype', { title: 'Prototype'});
+};
+
+exports.upload = function(req, res) {
+	res.render('../views/upload', { title: 'Upload'});
 };
