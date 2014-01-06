893e3f0613cc266b91fa4a48d01e1971b79bf635
Aristekrat <aristekrat@gmail.com>
2014-01-06 06:14:00 -0700

    Responsive bug fixes

--- config/mongo_config.js
+++ config/mongo_config.js
@@ -1,9 +1,10 @@
 var mongoose = require('../node_modules/mongoose');
-
+/*
 var username = process.env.MONGO_USERNAME; 
 var password = process.env.MONGO_PASSWORD; 
 var dbURL = 'mongodb://' + username + ':' + password + '@ds053838.mongolab.com:53838/heroku_app17845741';
-console.log(dbURL);
+*/
+var dbURL = 'mongodb://localhost/donateware';
 mongoose.connect(dbURL);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
